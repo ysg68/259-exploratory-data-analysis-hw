429d40f644312cfbb2eba53d3d30a5fe638e8059
John Franchak <franchak@gmail.com>
2022-02-03 08:45:47 -0800

    added exercise

---
+++ README.md
@@ -0,0 +1,41 @@
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+# 259-integrating-skills
+Exercise to put together all of the skills we've covered so far in class: importing and transforming data, manipulating strings, factors, and dates, writing functions, and using across, map, and for loops to automate operations.
+
+The data are drawn from the fivethirtyeight article [What 12 Months Of Record-Setting Temperatures Looks Like Across The U.S.](http://fivethirtyeight.com/features/what-12-months-of-record-setting-temperatures-looks-like-across-the-u-s/). The data are drawn from 10 different weather stations, each in a different US city.
+
+## Stations and city names
+
+STATION | CITY
+---|----
+KCLT | Charlotte
+KCQT | Los Angeles
+KHOU | Houston
+KIND | Indianapolis
+KJAX | Jacksonville
+KMDW | Chicago
+KNYC | New York City
+KPHL | Philadelphia
+KPHX | Phoenix
+KSEA | Seattle
+
+## Data
+
+Column | Description
+---|---------
+`date` | The date of the weather record, formatted YYYY-M-D
+`actual_mean_temp` | The measured average temperature for that day
+`actual_min_temp` | The measured minimum temperature for that day
+`actual_max_temp` | The measured maximum temperature for that day
+`average_min_temp` | The average minimum temperature on that day since 1880
+`average_max_temp` | The average maximum temperature on that day since 1880
+`record_min_temp` | The lowest ever temperature on that day since 1880
+`record_max_temp` | The highest ever temperature on that day since 1880
+`record_min_temp_year` | The year that the lowest ever temperature occurred
+`record_max_temp_year` | The year that the highest ever temperature occurred
+`actual_precipitation` | The measured amount of rain or snow for that day
+`average_precipitation` | The average amount of rain or snow on that day since 1880
+`record_precipitation` | The highest amount of rain or snow on that day since 1880
+
+Source: [Weather Underground](http://wunderground.com)
---
+++ exercise.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(lubridate)
+
+#> These data are drawn from the fivethirtyeight article:
+#> http://fivethirtyeight.com/features/what-12-months-of-record-setting-temperatures-looks-like-across-the-u-s/
+#> The directory us-weather-history contains a data file for each of 10 cities, labelled by their station name
+#> Each data file contains:
+#> `date` | The date of the weather record, formatted YYYY-M-D
+#> `actual_mean_temp` | The measured average temperature for that day
+#> `actual_min_temp` | The measured minimum temperature for that day
+#> `actual_max_temp` | The measured maximum temperature for that day
+#> `average_min_temp` | The average minimum temperature on that day since 1880
+#> `average_max_temp` | The average maximum temperature on that day since 1880
+#> `record_min_temp` | The lowest ever temperature on that day since 1880
+#> `record_max_temp` | The highest ever temperature on that day since 1880
+#> `record_min_temp_year` | The year that the lowest ever temperature occurred
+#> `record_max_temp_year` | The year that the highest ever temperature occurred
+#> `actual_precipitation` | The measured amount of rain or snow for that day
+#> `average_precipitation` | The average amount of rain or snow on that day since 1880
+#> `record_precipitation` | The highest amount of rain or snow on that day since 1880
+
+stations <- c("KCLT", "KCQT", "KHOU", "KIND", "KJAX", "KMDW", "KNYC", "KPHL", "KPHX", "KSEA")
+cities <- c("Charlotte", "Los Angeles", "Houston", "Indianapolis", "Jacksonville", 
+            "Chicago", "New York City", "Philadelphia", "Phoenix", "Seattle")
+
+
+
+#> Write a function that takes each station abbreviation and reads
+#> the data file and adds the station name in a column
+#> Make sure the date column is a date
+#> The function should return a tibble
+#> Call the function "read_weather" 
+#> Check by reading/glimpsing a single station's file
+
+
+
+
+#> Use map_dfr() and your new function to read in all 10 stations
+#> map_dfr() will take each dataframe and automatically bind them.
+#> Save the resulting dataset to "ds"
+
+
+
+
+#> Make a factor called "city" based on the station variable
+#> (station should be the level and city should be the label)
+#> Use fct_count to check that there are 365 days of data for each city 
+
+
+
+#> Since we're scientists, let's convert all the temperatures to C
+#> Write a function to convert F to C, and then use mutate across to 
+#> convert all of the temperatures, rounded to a tenth of a degree
+
+
+
+#> Write a function that counts the number of extreme temperature days,
+#> Where the actual min or max was equal to the (i.e., set the) record min/max
+#> Group the dataset by city to see how many extreme days each city experienced,
+#> and sort in descending order to show which city had the most
+
+
+
+#> Pull out the month from the date and make "month" a factor
+#> Split the tibble by month into a list of tibbles 
+
+
+
+#> For each month, determine the correlation between the actual_precipitation
+#> and the average_precipitation (across all cities), and between the actual and average mins/maxes
+#> Use a for loop, and print the month along with the resulting correlation
+
